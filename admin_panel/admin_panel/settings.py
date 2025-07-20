94356a410dd4dbd7e142c33a53bed416f45b485c
Mehdi <mehdirajabi@disroot.org>
2025-07-20 20:12:53 +0330

    Tweaks to the django settings

--- admin_panel/admin_panel/settings.py
+++ admin_panel/admin_panel/settings.py
@@ -128,11 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
