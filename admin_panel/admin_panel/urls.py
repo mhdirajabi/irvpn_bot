10f9de7d4cdb9845accfd2c116aa4bf42d737007
Mehdi <mehdirajabi@disroot.org>
2025-07-23 13:30:03 +0330

    Edit django urls file

--- admin_panel/admin_panel/urls.py
+++ admin_panel/admin_panel/urls.py
@@ -20,10 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("apps.core.urls", namespace="api")),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("apps.core.urls"))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
