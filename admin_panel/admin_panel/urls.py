b23775ade7b46ffd6ac535833341e2fe8b6b3f5d
Mehdi <mehdirajabi@disroot.org>
2025-07-23 13:22:42 +0330

    Edit django urls file

--- admin_panel/admin_panel/urls.py
+++ admin_panel/admin_panel/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/"),
-    include("apps.core.urls", namespace="api"),
+    path("api/", include("apps.core.urls", namespace="api")),
 ]
 
 if settings.DEBUG:
