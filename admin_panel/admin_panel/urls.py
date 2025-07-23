10c7d7e657957438893dc7f21ba28847480c5ae8
Mehdi <mehdirajabi@disroot.org>
2025-07-23 13:19:26 +0330

    Update django urls file

--- admin_panel/admin_panel/urls.py
+++ admin_panel/admin_panel/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/"),
+    include("apps.core.urls", namespace="api"),
 ]
 
 if settings.DEBUG:
