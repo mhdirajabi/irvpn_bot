6cd504f8bbe7bdf72736333cbe82adf2262a0679
Mehdi <mehdirajabi@disroot.org>
2025-07-26 16:51:45 +0330

    Fixed dispatcher logger issue

--- bot/middlewares/log_all_callbacks.py
+++ bot/middlewares/log_all_callbacks.py
@@ -2,9 +2,9 @@ from aiogram.types import Update
 from utils.logger import logger
 
 
-async def log_all_callbacks(update: Update, handler, **kwargs):
-    if update.callback_query:
+async def log_all_callbacks(handler, event: Update, data: dict):
+    if event.callback_query:
         logger.info(
-            f"Received callback query: {update.callback_query.data} from user {update.callback_query.from_user.id}"
+            f"Received callback query: {event.callback_query.data} from user {event.callback_query.from_user.id}"
         )
-    return await handler(update, **kwargs)
+    return await handler(event, data)
